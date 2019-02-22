1f2aa04ba65637d9778118eddd779c8c483663a2
parthkhanna150 <parth.khanna@mail.mcgill.ca>
2019-02-22 03:45:04 -0500

    q2 final code

--- Q_A/question1.py
+++ Q_A/question1.py
@@ -1,26 +1,41 @@
-# Question A
-#
-# Your goal for this question is to write a program that accepts two lines (x1,x2) and (x3,x4) on the x-axis and returns whether they overlap. As an example, (1,6) and (8,6) overlaps but not (1,5) and (6,8).
+"""
+Question A
+Your goal for this question is to write a program that accepts two lines (x1,x2) and (x3,x4) on
+the x-axis and returns whether they overlap. As an example, (1,6) and (8,6) overlaps but not (1,5) and (6,8).
+"""
+
+# swaps the numbers to ensure order
 def swap(a,b):
     t = a
     a = b
     b = t
     return a, b
 
+# checks if two lines overlap
 def are_overlap(x1,x2,x3,x4):
-# make sure the coordinates are sorted pairwise
+
+    # make sure the coordinates are ordered
     if x1>x2:
         x1, x2 = swap(x1,x2)
     if x3>x4:
         x3, x4 = swap(x3,x4)
-# checking if one line's either of the two coordinates lie(s) between the coordinates of the other line
-# this assumes x1>=x2 and x4>=x3 because of the swapping
+
+    """
+    now we just need to check if x2>=x3 and x4>=x1 for an overlap
+    this is because:
+    if x2<x3 -> x1,x2 coordinates are to the left of x3,x4
+    if x4<x1 -> x3, x4 coordinates are to the left of x1,x2
+    and thus lines dont overlapps
+    """
     return (x2>=x3) and (x4>=x1)
+
 
 if __name__ == '__main__':
 
     # keeps taking input until correct input is given
     while 1:
+
+        # handle exception for when input entered in a wrong format
         try:
             print("What are the x coordinates of the first line in x1,x2 format?")
             x1, x2 = tuple(int(x.strip()) for x in input().split(','))
@@ -28,7 +43,6 @@ if __name__ == '__main__':
             print("What are the x coordinates of the second line in x1,x2 format?")
             x3, x4 = tuple(int(x.strip()) for x in input().split(','))
 
-            # sorting the coordinates to correc inverse coordinates like (-1,9)
             result = are_overlap(x1, x2, x3, x4)
             print(result)
             break
---
+++ Q_B/question2.py
@@ -0,0 +1,61 @@
+"""
+Question B
+The goal of this question is to write a software library that accepts 2 version string as input
+and returns whether one is greater than, equal, or less than the other. As an example: “1.2” is
+greater than “1.1”. Please provide all test cases you could think of.
+"""
+
+# given two versions, returns which one is higher
+def compareVersion(v1, v2):
+
+    # split version strings into arrays
+    arr1 = v1.split(".")
+    arr2 = v2.split(".")
+    i = 0
+
+    # in a while loop compare the corresponding int values of the arrays
+    while(i < len(arr1) and i < len(arr2)):
+        if int(arr2[i]) > int(arr1[i]):
+            return -1
+
+        elif int(arr1[i]) > int(arr2[i]):
+            return 1
+        i += 1
+
+    # if nothing returned yet -> all numbers upto the last common index are equal
+    # next check, for whether there exist non-zero numbers after this index in each array
+    # copy value of i (last common index) in a temp variable
+    j = i
+
+    # check for presence of non-zero numbers in both arrays
+    if i==len(arr2) and i!=len(arr1):
+        while i < len(arr1):
+            if int(arr1[i])!=0:
+                return 1
+            i+=1
+
+    elif j!=len(arr2) and j==len(arr1):
+        while j < len(arr2):
+            if int(arr2[j])!=0:
+                return -1
+            j+=1
+
+    return 0
+
+
+if __name__ == '__main__':
+
+    try:
+        v1 = input("Enter version 1: ")
+        v2 = input("Enter version 2: ")
+
+        result = compareVersion(v1,v2)
+        if result == 0:
+            print("Same version numbers!")
+        elif result == 1:
+            print(v1," is higher than ",v2)
+        else:
+            print(v2," is higher than ",v1)
+
+    except Exception as e:
+        print(e)
